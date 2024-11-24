214b9b4bc2e2dd8a315de0ce247d2639ad9b1413
Suraj2017-cse <sahsuraj2001@gmail.com>
2024-11-24 15:54:26 +0530

    add download report

--- src/components/main/DownloadModal.js
+++ src/components/main/DownloadModal.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { TextField, Button, Grid, MenuItem, Select, FormControl, InputLabel, CircularProgress } from '@mui/material';
 // import { CloseSvg } from 'assets/images/Expand';
+import { useLocation } from 'react-router-dom';
 
 const DownloadModal = () => {
 
@@ -13,171 +14,158 @@ const isBase64Image = (image) => {
 const formatImageSrc = (image) => {
   return isBase64Image(image) ? image : `data:image/png;base64,${image}`;
 };
-  // const [date, setDate] = useState({
-  //   startDate: '',
-  //   endDate: '',
-  //   startTime: '',
-  //   endTime: '',
-  // });
-  // const [objectName, setObjectName] = useState('');
-  // const [loading, setLoading] = useState(false);
+const [data, setData] = useState([]);
+const [loading, setLoading] = useState(true); // Loading state for initial data fetch
+// const [error, setError] = useState(null); // Error state
+const [date, setDate] = useState({
+  startDate: '',
+  endDate: '',
+  startTime: '',
+  endTime: '',
+});
+const [objectName, setObjectName] = useState(''); // State for selected object
+const [searching, setSearching] = useState(false); // State to track search action
 
-  // suraj
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true); // Loading state for initial data fetch
-  // const [error, setError] = useState(null); // Error state
-  const [date, setDate] = useState({
-    startDate: '',
-    endDate: '',
-    startTime: '',
-    endTime: '',
-  });
-  const [objectName, setObjectName] = useState(''); // State for selected object
-  const [searching, setSearching] = useState(false); // State to track search action
+const [selectedPage, setSelectedPage] = useState(localStorage.getItem('selectedIndex') || 0); 
+const location = useLocation(); // Get current URL to detect changes in selected page
 
-  const [selectedPage, setSelectedPage] = useState(localStorage.getItem('selectedIndex') || 0); 
-  // const location = useLocation(); // Get current URL to detect changes in selected page
+// API endpoints for dynamic fetch
+const apiEndpoints = [
+  '', // Placeholder for non-searchable solutions
+  'https://jakson-cairo.online:8094/api/Employee/GetAnprData',
+  '',
+  'https://jakson-cairo.online:8094/api/Employee/GetAnprVechileDetaildata',
+  'https://jakson-cairo.online:8094/api/Fire/GetFire_Record',
+  'https://jakson-cairo.online:8094/api/Fire/GetDetection_Data',
+  'https://jakson-cairo.online:8094/api/Fire/Gethumanfall_Data',
+  'https://jakson-cairo.online:8094/api/Fire/GetOverGrass_Data',
+  'https://jakson-cairo.online:8094/api/Fire/GetCrowd_Record',
+  'https://jakson-cairo.online:8094/api/Fire/AnimalDetection_Data',
+  'https://jakson-cairo.online:8094/api/Fire/OverSpeed_Data',
+  '',
+];
 
-  // API endpoints for dynamic fetch
-  const apiEndpoints = [
-    '', // Placeholder for non-searchable solutions
-    'https://jakson-cairo.online:8094/api/Employee/GetAnprData',
-    '',
-    'https://jakson-cairo.online:8094/api/Employee/GetAnprVechileDetaildata',
-    'https://jakson-cairo.online:8094/api/Fire/GetFire_Record',
-    'https://jakson-cairo.online:8094/api/Fire/GetDetection_Data',
-    'https://jakson-cairo.online:8094/api/Fire/Gethumanfall_Data',
-    'https://jakson-cairo.online:8094/api/Fire/GetOverGrass_Data',
-    'https://jakson-cairo.online:8094/api/Fire/GetCrowd_Record',
-    'https://jakson-cairo.online:8094/api/Fire/AnimalDetection_Data',
-    'https://jakson-cairo.online:8094/api/Fire/OverSpeed_Data',
-    '',
-  ];
+// API search endpoints for dynamic fetch
+const searchApiEndpoints = {
+  0: '',
+  1: 'https://jakson-cairo.online:8094/api/Employee/GetAnprDataSerch',
+  2: 'https://jakson-cairo.online:8094/api/Employee/GetAnprVechileDetaildataserch',
+  3: '',
+  4: 'https://jakson-cairo.online:8094/api/Fire/GetFire_RecordSerch',
+  5: 'https://jakson-cairo.online:8094/api/Fire/GetDetection_Dataserch',
+  6: 'https://jakson-cairo.online:8094/api/Fire/Gethumanfall_DataSerch',
+  7: 'https://jakson-cairo.online:8094/api/Fire/GetOverGrass_Dataserch',
+  8: 'https://jakson-cairo.online:8094/api/Fire/GetCrowd_Recordserch',
+  9: 'https://jakson-cairo.online:8094/api/Fire/GetAnimal_DataSerch',
+};
 
-  // API search endpoints for dynamic fetch
-  const searchApiEndpoints = {
-    0: '',
-    1: 'https://jakson-cairo.online:8094/api/Employee/GetAnprDataSerch',
-    2: 'https://jakson-cairo.online:8094/api/Employee/GetAnprVechileDetaildataserch',
-    3: '',
-    4: 'https://jakson-cairo.online:8094/api/Fire/GetFire_RecordSerch',
-    5: 'https://jakson-cairo.online:8094/api/Fire/GetDetection_Dataserch',
-    6: 'https://jakson-cairo.online:8094/api/Fire/Gethumanfall_DataSerch',
-    7: 'https://jakson-cairo.online:8094/api/Fire/GetOverGrass_Dataserch',
-    8: 'https://jakson-cairo.online:8094/api/Fire/GetCrowd_Recordserch',
-    9: 'https://jakson-cairo.online:8094/api/Fire/GetAnimal_DataSerch',
-  };
-
-  console.log(`DownloadModal Data: ${data}`);
-  // console.log(`DownloadModal apiData: ${apiData}`);
 
   
-  // Fetch data based on selected index
-  const fetchData = async (index, filters = {}) => {
-    setLoading(true);
-    // setError(null); // Reset error state before new fetch
-    const apiEndpoint = index >= 0 && index <= 11 ? apiEndpoints[index] : '';
-    const searchEndpoint = searchApiEndpoints[index];
-    console.log(searchEndpoint);
-    console.log(apiEndpoint);
+// Fetch data based on selected index
+const fetchData = async (index, filters = {}) => {
+  setLoading(true);
+  // setError(null); // Reset error state before new fetch
+  const apiEndpoint = index >= 0 && index <= 11 ? apiEndpoints[index] : '';
+  const searchEndpoint = searchApiEndpoints[index];
+  console.log(searchEndpoint);
+  console.log(apiEndpoint);
+  
+  
+  
+  // Determine if search API should be called
+  const url = searchEndpoint && Object.keys(filters).length > 0
+    ? `${searchEndpoint}?date=${filters?.date}&camera=${filters?.camera}&time=${filters?.time}&objectname=${filters?.objectname}`
+    : apiEndpoint;
+
+  try {
+    // console.log(`Fetching data from: ${url}`);  // Log the URL being fetched
+
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+
+    const text = await response.text();
+    let apiData = null;
     
-    
-    
-    // Determine if search API should be called
-    const url = searchEndpoint && Object.keys(filters).length > 0
-      ? `${searchEndpoint}?date=${filters?.date}&camera=${filters?.camera}&time=${filters?.time}&objectname=${filters?.objectname}`
-      : apiEndpoint;
 
     try {
-      // console.log(`Fetching data from: ${url}`);  // Log the URL being fetched
-
-      const response = await fetch(url);
-      if (!response.ok) throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
-
-      const text = await response.text();
-      let apiData = null;
-      
-
-      try {
-        apiData = JSON.parse(text);  // Try parsing the response text into JSON
-        console.log("API Response:", apiData); // Log the raw API response
-      } catch (jsonError) {
-        throw new Error(`Failed to parse response: ${text}`);
-      }
-
-      // If the data is empty or null, handle that scenario
-      if (!apiData || apiData.length === 0) {
-        setData([]);
-        // setError("No Data Available");
-        return;
-      }
-
-      // Format the data
-      const formattedData = apiData?.map((item, index) => ({
-        image: item?.photo || item?.imagedata || 'N/A',
-        detection_type: item?.detection_type || item?.name || 'N/A',
-        camera_id: item?.camera_id || item?.camera || `Camera ${index + 1}`,
-        date: item?.date || 'Unknown Date',
-        time: item?.time || 'Unknown Time',
-      }));
-
-      setData(formattedData);
-
-    } catch (err) {
-      console.error("API Error:", err);
-      // setError(`Error fetching data: ${err.message}`);
-    } finally {
-      setLoading(false);
-      setSearching(false); // Reset searching state after fetch is complete
+      apiData = JSON.parse(text);  // Try parsing the response text into JSON
+      console.log("API Response:", apiData); // Log the raw API response
+    } catch (jsonError) {
+      throw new Error(`Failed to parse response: ${text}`);
     }
+
+    // If the data is empty or null, handle that scenario
+    if (!apiData || apiData.length === 0) {
+      setData([]);
+      // setError("No Data Available");
+      return;
+    }
+
+    // Format the data
+    const formattedData = apiData?.map((item, index) => ({
+      image: item?.photo || item?.imagedata || 'N/A',
+      detection_type: item?.detection_type || item?.name || 'N/A',
+      camera_id: item?.camera_id || item?.camera || `Camera ${index + 1}`,
+      date: item?.date || 'Unknown Date',
+      time: item?.time || 'Unknown Time',
+    }));
+
+    setData(formattedData);
+
+  } catch (err) {
+    console.error("API Error:", err);
+    // setError(`Error fetching data: ${err.message}`);
+  } finally {
+    setLoading(false);
+    setSearching(false); // Reset searching state after fetch is complete
+  }
+};
+
+// Fetch data on page load and when the selected page changes
+useEffect(() => {
+  const selectedIndex = parseInt(localStorage.getItem('selectedIndex')) || 0; // Default to 0 if not found
+  setSelectedPage(selectedIndex);
+  fetchData(selectedIndex); // Call fetchData with the selected index
+}, [location.pathname]);
+
+// Handle date change
+const handleDateChange = (e) => {
+  const { id, value } = e.target;
+  setDate((prevDate) => ({ ...prevDate, [id]: value }));
+};
+
+// Handle object selection change
+const handleObjectChange = (event) => {
+  setObjectName(event.target.value);
+};
+
+// Handle Search action
+const handleSearch = () => {
+  const filters = {
+    date: date.startDate,
+    camera: date.startTime,
+    time: date.endTime,
+    objectname: objectName,
   };
 
-  // Fetch data on page load and when the selected page changes
-  useEffect(() => {
-    const selectedIndex = parseInt(localStorage.getItem('selectedIndex')) || 0; // Default to 0 if not found
-    setSelectedPage(selectedIndex);
-    fetchData(selectedIndex); // Call fetchData with the selected index
-  }, [location.pathname]);
+  console.log("Filters sent to API:", filters); // Log the filters being sent to the API
 
-  // Handle date change
-  const handleDateChange = (e) => {
-    const { id, value } = e.target;
-    setDate((prevDate) => ({ ...prevDate, [id]: value }));
-  };
+  setSearching(true);  // Set searching state to true while search is in progress
+  fetchData(selectedPage, filters);  // Trigger the data fetch with selected filters
+};
 
-  // Handle object selection change
-  const handleObjectChange = (event) => {
-    setObjectName(event.target.value);
-  };
+  // useEffect(() => {
+  //   const today = new Date();
+  //   const formattedDate = today.toISOString().split('T')[0];
+  //   const formattedTime = today.toTimeString().split(' ')[0].slice(0, 5);
 
-  // Handle Search action
-  const handleSearch = () => {
-    const filters = {
-      date: date.startDate,
-      camera: date.startTime,
-      time: date.endTime,
-      objectname: objectName,
-    };
-
-    console.log("Filters sent to API:", filters); // Log the filters being sent to the API
-
-    // setSearching(true);  // Set searching state to true while search is in progress
-    fetchData(selectedPage, filters);  // Trigger the data fetch with selected filters
-  };
-  // suraj
-
-  useEffect(() => {
-    const today = new Date();
-    const formattedDate = today.toISOString().split('T')[0];
-    const formattedTime = today.toTimeString().split(' ')[0].slice(0, 5);
-
-    setDate({
-      startDate: formattedDate,
-      endDate: formattedDate,
-      startTime: formattedTime,
-      endTime: formattedTime,
-    });
-  }, []);
+  //   setDate({
+  //     startDate: formattedDate,
+  //     endDate: formattedDate,
+  //     startTime: formattedTime,
+  //     endTime: formattedTime,
+  //   });
+  // }, []);
 
   // const handleDateChange = e => {
   //   const { id, value } = e.target;
